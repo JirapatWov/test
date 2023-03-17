4d9625255ac033820a0e91511831f8f931660251
jirapat <jirapat.wov@gmail.com>
2023-03-17 16:00:54 +0700

    d

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const os = require('os');
+
+const networkInterfaces = os.networkInterfaces();
+const firstNetworkInterface = networkInterfaces[Object.keys(networkInterfaces)[0]];
+const ipAddress = firstNetworkInterface.find(interface => interface.family === 'IPv4').address;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
@@ -7,4 +12,5 @@ app.get('/', (req, res) => {
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
+  console.log(`IP address: ${ipAddress}`);
 });
